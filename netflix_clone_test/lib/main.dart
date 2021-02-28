b81777c60bad6fccb40b09c21a0bece676be2566
yoongyo <lykee2000@naver.com>
2021-02-28 16:12:49 +0900

    search_screen

--- netflix_clone_test/lib/main.dart
+++ netflix_clone_test/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:netflix_clone_test/screen/home_screen.dart';
 import 'package:netflix_clone_test/screen/more_screen.dart';
+import 'package:netflix_clone_test/screen/search_screen.dart';
 import 'package:netflix_clone_test/widget/bottom_bar.dart';
 
 void main() => runApp(MyApp());
@@ -27,11 +28,7 @@ class _MyAppState extends State<MyApp> {
             physics: NeverScrollableScrollPhysics(),
             children: <Widget>[
               HomeScreen(),
-              Container(
-                child: Center(
-                  child: Text('search'),
-                ),
-              ),
+              SearchScreen(),
               Container(
                 child: Center(
                   child: Text('save'),
